fb15f026b0228205383eb7430c5b9551cae970d7
Ziaeemehr <a.ziaeemehr@gmail.com>
2024-08-06 19:10:42 +0200

    Docs added.

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from unittest.mock import MagicMock as Mock
+from setuptools_scm import get_version
+
+# sys.path.insert(0,os.path.abspath("../examples"))
+sys.path.insert(0,os.path.abspath("../spikes"))
+sys.path.insert(0, os.path.abspath("../examples/"))
+
+needs_sphinx = '1.3'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
+    'numpydoc',
+    'sphinx.ext.graphviz',
+	'sphinx.ext.viewcode'
+	# 'nbsphinx'
+]
+
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'spikes'
+copyright = u'2024, Abolfazl Ziaeemehr'
+
+release = version = get_version(root='..', relative_to=__file__)
+
+default_role = "any"
+add_function_parentheses = True
+add_module_names = False
+html_theme = 'nature'
+pygments_style = 'colorful'
+# htmlhelp_basename = 'JiTCODEdoc'
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+numpydoc_show_class_members = False
+autodoc_member_order = 'bysource'
+graphviz_output_format = "svg"
+toc_object_entries_show_parents = 'hide'
+
+def on_missing_reference(app, env, node, contnode):
+	if node['reftype'] == 'any':
+		return contnode
+	else:
+		return None
+
+def setup(app):
+	app.connect('missing-reference', on_missing_reference)
---
+++ docs/index.rst
@@ -0,0 +1,38 @@
+Python codes for Spikes, decisions, and actions, Wilson 1999
+##################################################
+
+Installation, How to use
+=============================
+
+- using on Colab (Recommended)
+
+    - Go to examples
+    - Open a notebook and click on "open on colab"
+    - Uncomment the cell with pip install command to install the netsci package.
+
+- using on local machines
+
+.. code-block:: bash
+
+    pip3 install -e .
+    # or
+    pip install "git+https://github.com/Ziaeemehr/spikes.git" 
+
+
+Indices and tables
+-------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+API and documentation
+===========================
+
+
+.. automodule:: spikes
+    :members:
+    :undoc-members:
+
+
