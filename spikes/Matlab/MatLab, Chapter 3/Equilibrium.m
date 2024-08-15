f87d7d53df890d112387b6b0d28a0d01c15578bf
Ziaeemehr <a.ziaeemehr@gmail.com>
2024-08-15 10:18:48 +0200

    format Matlab code ch 3

--- spikes/Matlab/MatLab, Chapter 3/Equilibrium.m
+++ spikes/Matlab/MatLab, Chapter 3/Equilibrium.m
@@ -1,1 +1,4 @@
-%Solves for Steady State of Linear Nth order systemA = [-1 -0.5; 1 -1]B = [3 0]'Steady_State = -A\B
+%Solves for Steady State of Linear Nth order system
+A = [-1 -0.5; 1 -1]
+B = [3 0]'
+Steady_State = -A \ B
